4535fc8c8de451a3169eeaf5a84ce29901b1f9d2
Terence O'Donoghue <terence@creativelittledots.co.uk>
2018-07-20 20:27:59 +0100

    Update vc.config.php

--- config/vc.config.php
+++ config/vc.config.php
@@ -12,10 +12,6 @@
 	    | Tell the Service Provider which original VC Components to retain and support
 	    |
 	    */
-		
-	  	'reset_shortcodes' => true,
-	  	
-	  	'reset_styles' => true,
 	
 	    'support' => [
 			'vc_row',
@@ -29,7 +25,20 @@
 			'vc_section',
 			'gravityform',
 	    ],
+		
+	    /*
+	    |--------------------------------------------------------------------------
+	    | VC Reset
+	    |--------------------------------------------------------------------------
+	    |
+	    | Tell the Service Provider what to reset with Vc
+	    |
+	    */
 	    
+	    'reset_shortcodes' => true,
+		
+	    'reset_styles' => true,
+		
 	    /*
 	    |--------------------------------------------------------------------------
 	    | VC Params
@@ -65,20 +74,24 @@
 	    |
 	    */
 	
-	    'replace' => [
-		    
-		    	'/vc_row-fluid/' => 'o-grid--edge',
-			'/vc_row/' => 'o-grid',
-			'/wpb_row/' => '',
-			'/vc_inner/' => '',
-			'/wpb_column/' => 'o-grid__item',
-			'/vc_column_container/' => '',
-			'/vc_col-xs-(\d{1,2})/' => 'u-size-$1',
-			'/vc_col-sm-(\d{1,2})/' => 'u-size-$1-m',
-			'/vc_col-md-(\d{1,2})/' => 'u-size-$1-l',
-			'/vc_col-lg-(\d{1,2})/' => 'u-size-$1-xl'
-		
-	    ],
+	    'replace' => function( $class, $tag, $atts ) {
+            
+		    if ( $tag == 'vc_row' || $tag == 'vc_row_inner' ) {
+			    $classname = ! empty( $this->settings['replace']['vc_row'] ) ? $this->settings['replace']['vc_row'] : 'o-grid';
+			    $classname .= ! empty( $atts['full_width'] ) ? ( ! empty( $this->settings['replace']['vc_row-fluid'] ) ? $this->settings['replace']['vc_row-fluid'] : ' o-grid--edge' ) : '';
+			$class = str_replace( 'vc_row-fluid', $classname, $class );
+			$class = str_replace( array('vc_row', 'wpb_row', 'vc_inner'), array('', '', ''), $class );
+		    }
+		    if ( $tag == 'vc_column' || $tag == 'vc_column_inner' ) {
+			$class = str_replace( 'wpb_column vc_column_container', ! empty( $this->settings['replace']['wpb_column'] ) ? $this->settings['replace']['wpb_column'] : 'o-grid__item', $class );
+			$class = preg_replace( '/vc_col-xs-(\d{1,2})/', ! empty( $this->settings['replace']['vc_col-xs-$1'] ) ? $this->settings['replace']['vc_col-xs-$1'] : 'u-size-$1', $class );
+			$class = preg_replace( '/vc_col-sm-(\d{1,2})/', ! empty( $this->settings['replace']['vc_col-sm-$1'] ) ? $this->settings['replace']['vc_col-sm-$1'] : 'u-size-$1-m', $class );
+			$class = preg_replace( '/vc_col-md-(\d{1,2})/', ! empty( $this->settings['replace']['vc_col-md-$1'] ) ? $this->settings['replace']['vc_col-md-$1'] : 'u-size-$1-l', $class );
+			$class = preg_replace( '/vc_col-lg-(\d{1,2})/', ! empty( $this->settings['replace']['vc_col-lg-$1'] ) ? $this->settings['replace']['vc_col-lg-$1'] : 'u-size-$1-xl', $class );
+		    }
+		    return $class;
+
+		},
 	
 	    /*
 	    |--------------------------------------------------------------------------
@@ -92,6 +105,6 @@
 	    |
 	    */
 	
-	    'vc_path' => resources_path('vc')
+	    'vc_path' => resources_path('views/vc')
 	
 	];
